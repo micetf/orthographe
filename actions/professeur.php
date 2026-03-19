3ef55c1d64bf04a04a036fcabb51b464a979bc5f
MiCetF <webmaster@micetf.fr>
2026-03-19 22:28:35 +0100

    security(auth): supprimer le log des mots de passe en clair
    Les tentatives échouées sont désormais loguées avec horodatage
    mais sans le mot de passe. Réduit le risque d'exposition des
    credentials en cas d'accès non autorisé à datas/log.txt.

--- actions/professeur.php
+++ actions/professeur.php
@@ -1,22 +1,25 @@
 <?php
-function logErreurConnexion($login,$pwd) {
-  $s = file_get_contents('./datas/log.txt');
-  file_put_contents('./datas/log.txt',$s.PHP_EOL.$login.'-'.$pwd);
+
+function logErreurConnexion($login)
+{
+    $s = file_get_contents('./datas/log.txt');
+    file_put_contents('./datas/log.txt', $s.PHP_EOL.date('Y-m-d H:i:s').' LOGIN_ECHOUE: '.$login);
 }
 
-function is_professeur($infos) {
-  if (!empty($infos['loginP']) && !empty($infos['passwordP'])) {
-    $dir = dir('./datas/professeurs/');
-    while ($nom = $dir->read()) {
-      if (strpos($nom,'eo_'.md5($infos['loginP']).'_'.md5($infos['passwordP']).'_') === 0) {
-    		touch('./datas/professeurs/'.$nom);
-        return true;
-      } else if (strpos($nom,'eo_'.md5($infos['loginP']).'_'.md5(strtolower($infos['passwordP'])).'_') === 0) {
-        touch('./datas/professeurs/'.$nom);
-        return true;
-      }
+function is_professeur($infos)
+{
+    if (!empty($infos['loginP']) && !empty($infos['passwordP'])) {
+        $dir = dir('./datas/professeurs/');
+        while ($nom = $dir->read()) {
+            if (strpos($nom, 'eo_'.md5($infos['loginP']).'_'.md5($infos['passwordP']).'_') === 0) {
+                touch('./datas/professeurs/'.$nom);
+                return true;
+            } elseif (strpos($nom, 'eo_'.md5($infos['loginP']).'_'.md5(strtolower($infos['passwordP'])).'_') === 0) {
+                touch('./datas/professeurs/'.$nom);
+                return true;
+            }
+        }
     }
-  }
-  logErreurConnexion($infos['loginP'],$infos['passwordP']);
-  return false;
+    logErreurConnexion($infos['loginP']);
+    return false;
 }
